0e496b2d0d2d63f21d5e7b60c50637a50b85f8bc
joyci <xxjoycixx@gmail.com>
2024-03-03 10:43:09 -0300

    upload

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Syne } from "next/font/google";
 import "./globals.scss";
 import Header from "./sections/Header";
-import NavMobile from "./sections/NavMobile";
 
 const syne = Syne({ subsets: ["latin"] });
 
@@ -15,7 +14,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={syne.className}>
         <Header></Header>
-        <NavMobile></NavMobile>
         {children}
         </body>
     </html>
--- app/sections/Header.js
+++ app/sections/Header.js
@@ -5,6 +5,7 @@ import { usePathname } from 'next/navigation';
 
 import styles from './Header.module.scss';
 import Button from '../components/Button';
+import { Fragment } from 'react';
 
 export default function Header() {
 
@@ -20,10 +21,24 @@ export default function Header() {
     }
 
     return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <a className={styles.logo} href="#">Trote Solidário</a>
-                <nav className={styles.nav}>
+        <div className={styles.header_wrapper}>
+            <header className={styles.header}>
+                <div className={styles.container}>
+                    <a className={styles.logo} href="#">Trote Solidário</a>
+                    <nav className={styles.nav}>
+                        <Link href={hrefHome} className={pathName === hrefHome ? 'active' : 'disabled'}>Trote Solidário 2024</Link>
+                        <div className={styles.divider}></div>
+                        {/* <Link href={hrefEdicao2023} className={pathName === hrefEdicao2023 ? 'active' : 'disabled'}>Edição 2023</Link>
+                        <div className={styles.divider}></div> */}
+                        <Link href={hrefSobre} className={pathName === hrefSobre ? 'active' : 'disabled'}>Sobre</Link>
+                        <div className={styles.divider}></div>
+                        <Link href={hrefContato} className={pathName === hrefContato ? 'active' : 'disabled'}>Contato</Link>
+                    </nav>
+                    <Button section='header' href='#contribute'>Como contribuir?</Button>
+                    <div className={styles.menu} onClick={toggleMenu}></div>
+                </div>
+            </header>
+            <nav id='navmobile' className={styles.navmobile}>
                     <Link href={hrefHome} className={pathName === hrefHome ? 'active' : 'disabled'}>Trote Solidário 2024</Link>
                     <div className={styles.divider}></div>
                     {/* <Link href={hrefEdicao2023} className={pathName === hrefEdicao2023 ? 'active' : 'disabled'}>Edição 2023</Link>
@@ -31,10 +46,7 @@ export default function Header() {
                     <Link href={hrefSobre} className={pathName === hrefSobre ? 'active' : 'disabled'}>Sobre</Link>
                     <div className={styles.divider}></div>
                     <Link href={hrefContato} className={pathName === hrefContato ? 'active' : 'disabled'}>Contato</Link>
-                </nav>
-                <Button section='header' href='#contribute'>Como contribuir?</Button>
-                <div className={styles.menu} onClick={toggleMenu}></div>
-            </div>
-        </header>
+        </nav>
+        </div>
     )
 }
